760195b9df514206ecf8fe669d176aa4afa9c583
dongmingjuan <d387918156@gmail.com>
2017-08-12 08:58:37 +0800

    implement post

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -28,6 +28,8 @@ class GroupsController < ApplicationController
     end
   end
   def show
+    @group = Group.find(params[:id])
+    @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 3)
 
   end
   def destroy
