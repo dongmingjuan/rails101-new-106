49083c0a01a6cf96edcc10be862a805ed887e885
dongmingjuan <d387918156@gmail.com>
2017-08-12 09:53:33 +0800

    implement my groups & my posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,26 @@ class PostsController < ApplicationController
       render :new
     end
   end
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to group_path(@group)
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:alert] = 'POST DELETED'
+    redirect_to group_path(@group)
+  end
   private
   def post_params
     params.require(:post).permit(:content)
